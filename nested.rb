cb812b2e1f46bd8caaa1a4f39205655c987e1e6b
Timothy Rines <tsrines@gmail.com>
2019-09-23 02:26:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,14 +61,7 @@ def dennis_ritchies_language
 
 end
 
-def adding_matz  
-  matz_object = {
-  :yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  }
-}
-end
+
 # add the following information to the top level of programmer_hash
 
 
