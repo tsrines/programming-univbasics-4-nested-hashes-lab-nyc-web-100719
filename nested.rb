81d3b13f44ed4dc38752c6e316913b7e48dde4c0
Timothy Rines <tsrines@gmail.com>
2019-09-23 02:29:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,9 +74,15 @@ def adding_matz
 }
 end
 
-def unshift
-	programmer_hash = 
- 		{
+def adding_matz
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
+
+  programmer_hash = 
+    {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -90,7 +96,11 @@ def unshift
           :languages => ["C"]
         }
      }
-programmer_hash
+
+		 programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
     
 end
 
