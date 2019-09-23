acd1c3b1e43c7da8fb117bcb889377e401314164
Timothy Rines <tsrines@gmail.com>
2019-09-23 02:06:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie]to_S[:languages]
+     programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
